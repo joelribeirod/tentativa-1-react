434bf783ac8e44c6a007f43f127e3ed3febe7a19
Joel Ribeiro <joelribeirod2201@gmail.com>
2024-12-06 18:24:37 -0400

    pequenas alterações

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,7 +1,10 @@
+import {FaInstagram} from 'react-icons/fa'
+
 function Footer(){
     return (
         <footer>
             <a href="https://github.com/joelribeirod" target="blank">Criado por Joel Ribeiro &copy; 2024</a>
+            <a href="https://www.instagram.com/joelribeiro2215/" target="blank"><FaInstagram/></a>
         </footer>
     )
 }
--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom'
 function Nav(){
     return (
         <nav id='nav'>
-            <Link to="/" id='linkum'>Home</Link>
-            <Link to="/sobre" id='linkdois'>Sobre min</Link>
+            <Link to="/tentativa-1-react/" id='linkum'>Home</Link>
+            <Link to="/tentativa-1-react/sobre" id='linkdois'>Sobre min</Link>
         </nav>
     )
 }
